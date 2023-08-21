e7fec6ec87aef2afd2f0560563d37706ee241d67
ecortesg <cgarcia.ernesto@gmail.com>
2023-08-20 20:36:45 -0600

    Relocated radar chart label

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -11,38 +11,45 @@ import {
 
 function Diagram({ data }) {
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <RadarChart
-        cx="50%"
-        cy="50%"
-        outerRadius="75%"
-        data={data}
-        margin={{ bottom: 15 }}
-      >
-        <PolarGrid />
-        <PolarAngleAxis dataKey="position" />
-        <PolarRadiusAxis
-          angle={60}
-          label={{ value: "FPts/Game", position: "outside", offset: 25 }}
-        />
-        <Tooltip />
-        <Legend />
-        <Radar
-          name="League 1"
-          dataKey="avg1"
-          stroke="#14b8a6"
-          fill="#14b8a6"
-          fillOpacity={0.2}
-        />
-        <Radar
-          name="League 2"
-          dataKey="avg2"
-          stroke="#8b5cf6"
-          fill="#8b5cf6"
-          fillOpacity={0.2}
-        />
-      </RadarChart>
-    </ResponsiveContainer>
+    <>
+      <div className="mt-4 text-center">
+        <h3>FPts/Game</h3>
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <RadarChart
+          cx="50%"
+          cy="50%"
+          outerRadius="80%"
+          data={data}
+          margin={{
+            top: 16,
+            right: 24,
+            left: 24,
+            bottom: 16,
+          }}
+        >
+          <PolarGrid />
+          <PolarAngleAxis dataKey="position" />
+          <PolarRadiusAxis angle={60} />
+          <Tooltip />
+          <Legend />
+          <Radar
+            name="League 1"
+            dataKey="avg1"
+            stroke="#14b8a6"
+            fill="#14b8a6"
+            fillOpacity={0.2}
+          />
+          <Radar
+            name="League 2"
+            dataKey="avg2"
+            stroke="#8b5cf6"
+            fill="#8b5cf6"
+            fillOpacity={0.2}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
