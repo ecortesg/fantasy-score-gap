ca11245ff69bc5f309465b1e494f39cf6ade1227
ecortesg <cgarcia.ernesto@gmail.com>
2023-10-10 21:53:08 -0600

    Changed loading icon

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,9 @@
-import { BiLoaderAlt } from "react-icons/bi";
+import { AiOutlineLoading } from "react-icons/ai";
 
 export function LoadingSpinner() {
   return (
     <div className="flex justify-center items-center p-2 h-full">
-      <BiLoaderAlt className="animate-spin w-16 h-16" />
+      <AiOutlineLoading className="animate-spin w-16 h-16" />
     </div>
   );
 }
