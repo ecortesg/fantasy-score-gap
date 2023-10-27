b15bbdaa88219a770ba1b3d7e5af4cb467ee82c0
ecortesg <cgarcia.ernesto@gmail.com>
2023-10-27 00:15:44 -0600

    Refactored select options

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,9 +52,14 @@ function Header() {
           onChange={handleChange1}
           className="border rounded bg-slate-200 font-bold px-2 py-1"
         >
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
+          {Array.from(
+            { length: 2022 - 2020 + 1 },
+            (_, index) => 2020 + index
+          ).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
         </select>
         <select
           name="week"
@@ -64,24 +69,11 @@ function Header() {
           className="border rounded bg-slate-200 font-bold px-2 py-1"
         >
           <option value="season">Season</option>
-          <option value={1}>Week 1</option>
-          <option value={2}>Week 2</option>
-          <option value={3}>Week 3</option>
-          <option value={4}>Week 4</option>
-          <option value={5}>Week 5</option>
-          <option value={6}>Week 6</option>
-          <option value={7}>Week 7</option>
-          <option value={8}>Week 8</option>
-          <option value={9}>Week 9</option>
-          <option value={10}>Week 10</option>
-          <option value={11}>Week 11</option>
-          <option value={12}>Week 12</option>
-          <option value={13}>Week 13</option>
-          <option value={14}>Week 14</option>
-          <option value={15}>Week 15</option>
-          <option value={16}>Week 16</option>
-          <option value={17}>Week 17</option>
-          <option value={18}>Week 18</option>
+          {[...Array(18)].map((_, index) => (
+            <option key={index + 1} value={index + 1}>
+              Week {index + 1}
+            </option>
+          ))}
         </select>
       </div>
       <div className="flex flex-wrap justify-center gap-2 xl:gap-4">
