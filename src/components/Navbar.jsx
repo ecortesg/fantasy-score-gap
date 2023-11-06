036d9869dc1770a9ae272788a1768f3993bb6f19
ecortesg <cgarcia.ernesto@gmail.com>
2023-11-05 22:28:24 -0600

    Added Ko-Fi  link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,32 +33,45 @@ function Navbar({ openSidebar, onIconClick }) {
         {!openSidebar && (
           <IoMdMenu
             className="cursor-pointer mr-4 2xl:hidden"
-            size={28}
+            size={32}
             onClick={onIconClick}
           />
         )}
         {openSidebar && (
           <IoMdClose
             className="cursor-pointer mr-4 2xl:hidden"
-            size={28}
+            size={32}
             onClick={onIconClick}
           />
         )}
         <h1 className="font-bold text-lg">Fantasy Score Gap</h1>
       </div>
-      {theme === "dark" ? (
-        <MdOutlineDarkMode
-          className="cursor-pointer hover:bg-indigo-400 rounded-full p-2"
-          size={42}
-          onClick={() => changeTheme()}
-        />
-      ) : (
-        <MdOutlineLightMode
-          className="cursor-pointer hover:bg-blue-400 rounded-full p-2"
-          size={42}
-          onClick={() => changeTheme()}
-        />
-      )}
+      <div className="flex items-center justify-between">
+        <a
+          href="https://ko-fi.com/S6S8QVBNF"
+          target="_blank"
+          className="cursor-pointer h-14"
+        >
+          <img
+            src="kofi-logo.png"
+            alt="Buy Me a Coffee at ko-fi.com"
+            className="h-full"
+          />
+        </a>
+        {theme === "dark" ? (
+          <MdOutlineDarkMode
+            className="cursor-pointer"
+            size={32}
+            onClick={() => changeTheme()}
+          />
+        ) : (
+          <MdOutlineLightMode
+            className="cursor-pointer"
+            size={32}
+            onClick={() => changeTheme()}
+          />
+        )}
+      </div>
     </nav>
   );
 }
